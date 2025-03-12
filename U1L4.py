5bb2d2720acabbb03d4166f144ae01cf190734cd
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-03-12 07:13:02 -0500

    Create U1L4.py

---
+++ U1L4.py
@@ -0,0 +1,19 @@
+# Explanation of logical operators and, or, not
+
+# Logical AND - returns True if both operands are True, otherwise False
+x = 5
+y = 10
+print(x > 0 and y < 20)
+#print(x > 0 and y < 20) #try and change this statement so it becomes false
+
+# Logical OR - returns True if at least one operand is True, otherwise False
+x = 5
+y = 10
+print(x > 0 or y < 0)
+#print(x > 0 or y < 0) #try and change this statement so it stays true but for y instead of x
+
+# Logical NOT - returns True if the operand is False, and vice versa
+x = True
+print(not x)
+print(x)
+

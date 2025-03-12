41d549a9036a71fde0b12873fd2635be423f2926
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-03-12 07:39:26 -0500

    Create U4L2.py

---
+++ Unit_4/U4L2.py
@@ -0,0 +1,21 @@
+# Example 1: Print numbers from 1 to 10
+print("Example 1:")
+for i in range(1, 11):
+    print(i)
+
+# Example 2: Print even numbers from 2 to 20
+print("\nExample 2:")
+for i in range(2, 21, 2):  # Increment by 2 to get even numbers
+    print(i)
+
+# Example 3: Print numbers from 10 to 1 in reverse order
+print("\nExample 3:")
+for i in range(10, 0, -1):  # Decrement by 1 to reverse the order
+    print(i)
+
+# Example 4: Print square of numbers from 1 to 5
+print("\nExample 4:")
+for i in range(1, 6):
+    print(i, "squared is", i**2)
+
+

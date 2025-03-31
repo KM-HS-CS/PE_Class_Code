a39e7ea65a0f638965f91dcfe7ba9e3e2e9c5bd1
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-03-31 07:29:14 -0500

    Create receipt.py

---
+++ Unit_1/U2L5/receipt.py
@@ -0,0 +1,32 @@
+##################
+#  U1L5 Program  #
+##################
+
+"""Your task for the assigngment is as follows:
+     1. Create a variable and store the response 
+        to the user's total for their bill
+     2. Create a variable and store the response 
+        to the user's tip amount
+     3. Display a receipt that show the total, tip, 
+        tax, and grand total, as well as if the the tip 
+        was over 15% The output should look like this:
+           a. remember % go to decimals by moving 2 places
+              so 15% is .15
+           b. tax is 5.5% so .055
+           c. do not worry about extra decimals as you see in
+              this example under tax 
+           d. remember for the tests, spacing matters, but you
+              can still score perfect if they fail as long as
+              spacing is the only issue: Meaning --
+                   Sub Total: $ 10.23 would mark correct, but
+                   Sub Total:$10.23 would be marked wrong
+        
+        =====================
+        Sub Total: $ 10.23
+        Tip Amt: $ 2.00
+        Over 15%:  True
+        Tax: $ 0.56265
+        ----------------------
+        Grand Total: $ 12.79265 """
+
+#### Start Your Code here #### 

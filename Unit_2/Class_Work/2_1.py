eba874749fb02ba4e3ff9190ce63d85d54ce7efb
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:05:50 -0500

    Create 2_1.py

---
+++ Unit_2/Class_Work/2_1.py
@@ -0,0 +1,8 @@
+#Write a program that will ask the user for their current percentage (0-100). Then Print out the letter grade based on this data:
+##90-100	A
+##80-90	  B
+##70-80	  C
+##60-70	  D
+##0-59	  F
+##
+##Output “Not a valid input” otherwise

f6ce8625d648a54cc1fabeb69bcfa644fe71b814
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:10:49 -0500

    Create 3_1.py

---
+++ Unit_3/Class_Work/3_1.py
@@ -0,0 +1,23 @@
+#1: Create a string that contains your name called myName. Then, print the 1st and last letter of your name. 
+
+#2: Create a string called favAnimal that contains your favorite animal.
+##a. Using .upper, print favAnimal so it’s all uppercase
+
+##b. Using .lower, print favAnimal so it’s all lowercase
+
+##c. Print the length of the string
+
+
+#3: Create a string that contains your favourite color and then print it reversed! 
+
+#4: Using str1 and slicing, print only "coding" from str1
+# Do not touch this code:
+str1 = "I love coding"
+
+#5: Using str1 and slicing, print only "Hello" from str1
+# Do not touch this code:
+str1 = "Hello world!"
+
+
+
+

e203ea482df4c2de2496597ad1376f9ec9312661
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:01:54 -0500

    Create 1-3.py

---
+++ Unit_1/Class_Work/1-3.py
@@ -0,0 +1,9 @@
+# 6. Ask the user to enter their favorite color using input and store it in a variable called userColor. Then print that color.
+
+# 7. Ask the user to enter their lucky number using input (remember to cast it to an int) then store that in a variable called userNumber.
+
+# 8. Ask the user for a number using input (remember to cast it to an int), store it in a variable called num and then add 10 to it. Then print num.
+
+# 8. Ask the user for the year that they were born in using input and store it in a variable called birthYear. Then, print their current age. 
+
+# 10. Pretend you are a cashier at a grocery store. Using input, ask the user for their total amount. Calculate the tax on the amount (assuming the tax is 5% -> total*1.05). Print the total including the tax.

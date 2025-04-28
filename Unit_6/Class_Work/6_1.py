b497d01750abb6c4a6bb24e935afe4ef3a064850
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:32:39 -0500

    Update 6_1.py

--- Unit_6/Class_Work/6_1.py
+++ Unit_6/Class_Work/6_1.py
@@ -1,1 +1,5 @@
+# 1. Create a text file called words.txt with 20 different words. Write a program that reads words.txt and prints only the words with more than 10 characters (not counting whitespace). Use a function to check and print the words.
 
+# 2. Write a Python program to count the number of lines in a text file.
+
+# 3. Write a Python program to read the contents of a file and count the number of occurrences of a specific word.

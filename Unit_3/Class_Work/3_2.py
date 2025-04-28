0e159ba21d673af8ae68e32bd5969f844d267f50
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:12:38 -0500

    Create 3_2.py

---
+++ Unit_3/Class_Work/3_2.py
@@ -0,0 +1,34 @@
+#1. Create a list that contains 5 of your favourite hobbies.
+ ##a. Print the list. 
+
+ ##b. Print the length of the list.
+ 
+ ##c. Print index 2.
+ 
+ ##d. Print the first item (use indexing).
+
+#2. Create a list of 5 of your favourite animals called favAnimals.
+ ##a. Print the length of the list
+
+ ##b. Print index 2
+
+ ##c. Print the last item 
+
+ ##d. Append your name to it
+
+ ##e. Insert your age at index 2
+
+ ##f. Remove one item from your list
+
+#3. Combine list1 and list2 into a new list called list3. Then, print list3.
+list1 = ["pumpkin", "halloween", "jack-o-lanter", "spooky"]
+list2 = ["christmas", "new year", "holiday", "santa", “snow”]
+
+
+
+
+
+
+
+
+

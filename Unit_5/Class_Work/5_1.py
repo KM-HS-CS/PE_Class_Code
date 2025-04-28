2681fd3835bc58567bf27525f61ddf8d0f8ef106
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:31:02 -0500

    Create 5_1.py

---
+++ Unit_5/Class_Work/5_1.py
@@ -0,0 +1,14 @@
+# 1. Write a simply Person class with attributes of name and age and a method of greet() that prints the following Message:
+### Hello, my name is {name} and I am {age} years old. 
+
+
+# 2. Using that class, create 5 historical figures and have them greet the user. 
+
+
+
+
+
+
+
+
+

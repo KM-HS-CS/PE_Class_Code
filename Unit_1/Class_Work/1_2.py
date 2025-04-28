0b80f7872a19a6696fbc7c57642111fc2ec46e57
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 07:59:36 -0500

    Update and rename 3_2.py to 1_2.py

---
+++ Unit_1/Class_Work/1_2.py
@@ -0,0 +1,19 @@
+# 1. You must define the following variables:
+#a. An int called luckyNumber which contains your favourite number
+
+#b. A string called favouriteFood which contains your favourite food
+
+#c. A boolean called iCanSki and set it equal to either True or False (if you can ski)
+
+# 2. Create a variable named favAnimal and set it equal to your favorite animal. Then print favAnimal to the console.
+
+# 3. Create a variable named evenNumber and set it equal to an even number. Then create another variable named oddNumber and set it equal to an odd number.
+
+# 4. Create a variable named iLikeIceCream and set it equal to a boolean (either True or False).
+
+# 5. Create a variable named phoneNumber and set it equal to a random phone number.
+
+
+
+
+

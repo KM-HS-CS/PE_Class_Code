40ecc46ccb00cbc95c72fe89b2ca0d5246e5c2b2
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:06:45 -0500

    Create 2_3.py

---
+++ Unit_2/Class_Work/2_3.py
@@ -0,0 +1,9 @@
+# 1. Ticket prices for a senior (over 65) is $4.75, prices for kids 12 and younger is $4.25, and everyone else is $6.50. Write a program which gives the correct price based on the age. 
+
+# 2. Ask for an integer, print out it if it even or odd.
+
+# 3. Write a fizz buzz program. If the number is a multiple of three print “Fizz”, if it is a multiple of five print “Buzz” and if it is both print “FizzBuzz”
+
+# 4. Write a program which asks for a number and prints if it is positive, negative, or zero.
+
+# 5. Write a program that asks for a letter and prints if it is a vowle or not. (Vowels are a, e, i, o, and u)

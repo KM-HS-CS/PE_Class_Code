e4d31394894e5fb314758bd2bde86b3a0b773dd8
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:28:36 -0500

    Create 4_1.py

---
+++ Unit_4/Class_Work/4_1.py
@@ -0,0 +1,27 @@
+# 1, Write a while loop:
+##While num is smaller than 10, increase num by 1 and print it to the console
+##Do not touch this code:
+num = 2
+##Start Here##
+
+
+# 2. Write a while loop:
+##While x is smaller than 500, multiply x by 5 and print it to the console
+##Do not touch this code:
+x = 3
+##Start Here##
+
+
+# 3. Write a while loop:
+##While i is bigger than 5, subtract 10 from i and print it 
+##Do not touch this code:
+i = 100
+##Start Here##
+
+
+# 4. Write a while loop:
+##While y is bigger than 1, divide y by 2 and print it
+##Do not touch this code:
+y = 1000
+##Start Here##
+

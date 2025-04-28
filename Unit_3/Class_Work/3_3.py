28215088e5da54a41492c3894c844c201146306d
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:13:11 -0500

    Create 3_3.py

---
+++ Unit_3/Class_Work/3_3.py
@@ -0,0 +1,8 @@
+##Make a program that calculates your grade.
+#Store a list that has a minimum of 10 grades
+#Come up with the letter grade, as well as the best score, and worst score
+
+##Example:
+##scores = [5,3,4,4,2,5]
+##max = 5, min = 2, Grade = B  (76%)
+

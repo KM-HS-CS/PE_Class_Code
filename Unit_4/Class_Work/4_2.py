d6a7fcb43e444134fb5a669c8a3b1541013f1735
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:29:28 -0500

    Create 4_2.py

---
+++ Unit_4/Class_Work/4_2.py
@@ -0,0 +1,7 @@
+#1. Write a Python program that will use a for loop to print the even numbers from 2 to 100, inclusive.
+
+#2. Ask the user for a range (upper and lower bound), print the sum of numbers within that range
+
+#3. Write a for loop that prints numbers from the range of 0-10 (inclusive)
+
+#4. Write a for loop that prints numbers from the range of 5-25 (inclusive)

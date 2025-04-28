c647bce3920b14619de5777ae3da711ae9bd7dc9
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:30:13 -0500

    Create 4_4.py

---
+++ Unit_4/Class_Work/4_4.py
@@ -0,0 +1,20 @@
+#1. Write a function called MinutesToSeconds that converts minutes into seconds. The parameter is an integer called minutes. Print the seconds.
+
+#2. Create a function that takes two arguments. Both arguments are integers, a and b. Print true if one of them is 10 or if their sum is 10.
+#### Examples:
+#### makesTen(9, 10) ➞ true
+#### makesTen(9, 9) ➞ false
+#### makesTen(1, 9) ➞ true
+
+#3. Given a string, print true if its length is even or false if the length is odd.
+
+#4. Create a function that takes a word and prints the new word without including the first character.
+
+
+
+
+
+
+
+
+

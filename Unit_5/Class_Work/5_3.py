3c090aecd2bf41b199084ffe8bcd2adc89c706b4
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:31:32 -0500

    Create 5_3.py

---
+++ Unit_5/Class_Work/5_3.py
@@ -0,0 +1,4 @@
+# 1. Write a dataclass for a rectangle. It should have a width and length and methods to calculate the area, perimeter, and diagonal. 
+
+# 2. Using that dataclass, create 5 rectangles and print out all of their data using only 5 print statements. 
+

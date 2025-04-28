6c3cf72431de466f8a095dc978b300e4b3160607
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:06:23 -0500

    Create 2_2.py

---
+++ Unit_2/Class_Work/2_2.py
@@ -0,0 +1,9 @@
+# 1. If num1 is greater than num2, print “Num is small”
+
+# 2. If age >= 18 print “You can vote”
+
+# 3. If snowOutside equals True print “Let’s go sledding!”
+
+# 4. snowDay is True print “No School Today” otherwise print “We have school today”
+
+# 5. If tired is true print “I will go to sleep” otherwise print “I will sleep later”

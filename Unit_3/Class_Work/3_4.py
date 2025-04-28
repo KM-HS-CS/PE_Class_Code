e350d5ba1cd19824d5f4f6065796399879fa3669
Mr. Logan <77634091+loganb-kmhs@users.noreply.github.com>
2025-04-28 08:17:10 -0500

    Create 3_4.py

---
+++ Unit_3/Class_Work/3_4.py
@@ -0,0 +1,57 @@
+#################
+##Dictionaries ##
+#################
+# 1. Create a dictionary called aboutMe that contains the following keys and values: 
+   ## Name (string)
+   ## Age (number)
+   ## favFood (list)
+   ## hairColor (string)
+   ## favSubject (string)
+   ## favSweet (string)
+   ## favHobbies (list)
+   ## likeCoding (boolean)
+   ## Then, print aboutMe
+
+# 2. Create a dictionary called heights that 5 student names (keys) and their heights (values). 
+   ## A. Print the dictionary
+   ## B. Print the second value
+   ## C. Remove the last key-value pair then print the dictionary
+   ## D. Add one new key-value pair then print the dictionary
+   ## E. Print the length of your dictionary
+   ## F. Change the value of one existing value then print the dictionary
+
+
+############
+## Tuples ##
+############
+
+# 1. Create a tuple with a single item 50
+
+# 2. Access the value 4 and print it from tuple1
+tuple1 = ("Purple", "Pink", 17, [1, 2, 3, 4, 5], (5, 15, 25))
+
+# 3. Create a new tuple with 6 foods called foods.
+   # a. Print the tuple
+   # b. Print the length
+   # c. Print the first item in your tuple
+   # d. Print the last item in your tuple
+
+##########
+## Sets ##
+##########
+# 1. Create a random set of 5 sports called sports.
+   ## a. Print the set
+   ## b. Print the length of a set
+   ## c. Add the string “HELLO” to your set
+   ## d. Remove the first item in your set.
+
+
+# 2. Given this set: 
+mySet = {"Blue", "Purple", "Pink", "Orange", "Blue", "Pink"}
+   ## a. Print mySet - using a comment, explain what you notice? What is wrong with mySet?
+
+
+
+
+
+
